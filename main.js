5e9edc7a0e4782ebcad94d20b496c183ca322a33
LeShawn Edouard <leshawnmedouard@gmail.com>
2019-12-23 14:29:41 -0500

    Singleton Design Pattern example

--- main.js
+++ main.js
@@ -1,39 +1,72 @@
-//********** Factory Method Design Pattern **********//
+// //********** Factory Method Design Pattern **********//
 
-function Developer(name) {
-    this.name = name
-    this.type = "Developer"
+// function Developer(name) {
+//     this.name = name
+//     this.type = "Developer"
+// }
+
+// function Tester(name) {
+//     this.name = name
+//     this.type = "Tester"
+// }
+
+// function EmployeeFactory() {
+//     this.create = (name, type) => {
+//        switch(type) {
+//             case 1:
+//                return new Developer(name)
+//                break;
+//             case 2:
+//                 return new Tester(name)
+//                 break;
+//        }
+//     }
+// }
+
+// function say() {
+//     console.log("Hi, I am " + this.name + " and I am a " + this.type)
+// }
+
+// const employeeFactory = new EmployeeFactory()
+// const employees = [];
+
+// employees.push(employeeFactory.create("Patrick", 2))
+// employees.push(employeeFactory.create("LeShawn", 1))
+// employees.push(employeeFactory.create("Alfred", 2))
+
+// employees.forEach( emp => {
+//     say.call(emp)
+// })
+
+
+//********** Singleton Design Pattern **********//
+
+function Process(state) {
+    this.state = state
 }
 
-function Tester(name) {
-    this.name = name
-    this.type = "Tester"
-}
-
-function EmployeeFactory() {
-    this.create = (name, type) => {
-       switch(type) {
-            case 1:
-               return new Developer(name)
-               break;
-            case 2:
-                return new Tester(name)
-                break;
-       }
+const Singleton = (function() {
+    function ProcessManager() {
+        this.numProcess = 0
     }
-}
 
-function say() {
-    console.log("Hi, I am " + this.name + " and I am a " + this.type)
-}
+    let pManager
 
-const employeeFactory = new EmployeeFactory()
-const employees = [];
+    function createProcessManager() {
+        pManager = new ProcessManager()
+        return pManager
+    }
 
-employees.push(employeeFactory.create("Patrick", 2))
-employees.push(employeeFactory.create("LeShawn", 1))
-employees.push(employeeFactory.create("Alfred", 2))
+    return {
+        getProcessManager: () => {
+            if(!pManager)
+            pManager = createProcessManager()
+            return pManager
+        }
+    }
+})()
 
-employees.forEach( emp => {
-    say.call(emp)
-})
+const processManager = Singleton.getProcessManager();
+const processManager2 = Singleton.getProcessManager();
+
+console.log(processManager === processManager2)
