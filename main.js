f0aa8231154ec7692816dc4fdd8a9491f14e84da
LeShawn Edouard <leshawnmedouard@gmail.com>
2020-01-11 16:55:55 -0500

    Created unsubscribe method to remove an observer function from the observer array

--- main.js
+++ main.js
@@ -194,4 +194,10 @@ Subject.prototype = [
     subscribe: function(fn) {
         this.observers.push(fn)
     },
+    unsubscribe: function(fnToRemove) {
+        this.observers = this.observers.filter( fn => {
+            if(fn != fnToRemove)
+            return fn
+        })
+    }
 ]
