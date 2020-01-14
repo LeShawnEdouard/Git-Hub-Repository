975b66489733a17105eff7f33d9253d1e146ef36
LeShawn Edouard <leshawnmedouard@gmail.com>
2020-01-13 19:30:57 -0500

    Created methods to set age, phone, and address

--- main.js
+++ main.js
@@ -255,4 +255,19 @@ class UserBuilder {
     constructor(name) {
         this.user = new User(name)
     }
+
+    setAge(age) {
+        this.user.age = age
+        return this
+    }
+
+    setPhone(phone) {
+        this.user.phone = phone
+        return this
+    }
+
+    setAddress(address) {
+        this.user.address = address
+        return this
+    }
 }
