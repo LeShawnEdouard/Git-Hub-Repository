29bd32198d8f607c1b3d5f503c4bdb3af69461e1
LeShawn Edouard <leshawnmedouard@gmail.com>
2020-01-13 20:05:46 -0500

    Refactor the 'User' class and passed the last three parameters into one object

--- main.js
+++ main.js
@@ -276,5 +276,26 @@ class UserBuilder {
     }
 }
 
-let builder = new UserBuilder("LeShawn").build()
+let user = new UserBuilder("LeShawn").setAddress().setAge(10).build()
+console.log(user)
+
+// OR - 
+
+class Address {
+    constructor(zip, street) {
+        this.zip = zip
+        this.street = street
+    }
+}
+
+class User {
+    constructor(name, {age, phone, address} = {}) {
+        this.name = name
+        this.age = age
+        this.phone = phone
+        this.address = address
+    }
+}
+
+let user = new User("LeShawn", {age:10, address: new Address(27610, "Raleigh")})
 console.log(user)
