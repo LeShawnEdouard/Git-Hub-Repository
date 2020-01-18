dbfd138070d2721d6c825fa49dd961ddaef0112c
LeShawn Edouard <leshawnmedouard@gmail.com>
2020-01-18 12:38:00 -0500

    Commented out builder example

--- main.js
+++ main.js
@@ -237,49 +237,49 @@ subject.fire()
 // by name and other methods that will return the users age, phone, and 
 // address (zip, street).
 
-class Address {
-    constructor(zip, street) {
-        this.zip = zip
-        this.street = street
-    }
-}
+// class Address {
+//     constructor(zip, street) {
+//         this.zip = zip
+//         this.street = street
+//     }
+// }
 
-class User {
-    constructor(name, age, phone, address) {
-        this.name = name
-        this.age = age
-        this.phone = phone
-        this.address = address
-    }
-}
+// class User {
+//     constructor(name, age, phone, address) {
+//         this.name = name
+//         this.age = age
+//         this.phone = phone
+//         this.address = address
+//     }
+// }
 
-class UserBuilder {
-    constructor(name) {
-        this.user = new User(name)
-    }
+// class UserBuilder {
+//     constructor(name) {
+//         this.user = new User(name)
+//     }
 
-    setAge(age) {
-        this.user.age = age
-        return this
-    }
+//     setAge(age) {
+//         this.user.age = age
+//         return this
+//     }
 
-    setPhone(phone) {
-        this.user.phone = phone
-        return this
-    }
+//     setPhone(phone) {
+//         this.user.phone = phone
+//         return this
+//     }
 
-    setAddress(address) {
-        this.user.address = address
-        return this
-    }
+//     setAddress(address) {
+//         this.user.address = address
+//         return this
+//     }
 
-    build() {
-        return this.user
-    }
-}
+//     build() {
+//         return this.user
+//     }
+// }
 
-let user = new UserBuilder("LeShawn").setAddress().setAge(10).build()
-console.log(user)
+// let user = new UserBuilder("LeShawn").setAddress().setAge(10).build()
+// console.log(user)
 
 // OR - 
 
